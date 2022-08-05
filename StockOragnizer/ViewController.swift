600d958b087d09ca45ea3f8f8169ad6c1343604f
Gi Wook Lee <108044639+chad0909@users.noreply.github.com>
2022-08-05 16:03:05 +0900

    Initial Commit

---
+++ StockOragnizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockOragnizer
+//
+//  Created by 077tech on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
